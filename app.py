91e278c1d18a870b741c1d41f7084d784639729e
Ashran Haider <ashranhaider@hotmail.com>
2024-06-09 18:56:49 -0400

    demo actions

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ DATABASE = os.environ.get("DATABASE") or "employees"
 COLOR_FROM_ENV = os.environ.get('APP_COLOR') or "lime"
 DBPORT = int(os.environ.get("DBPORT"))
 
-
+# demo change
 
 # Create a connection to the MySQL database
 db_conn = connections.Connection(
